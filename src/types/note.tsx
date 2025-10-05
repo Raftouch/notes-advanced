eb12df9380a4e32952f26674f3d20336c3f34f69
raftouch <rafanelkin@gmail.com>
2025-10-05 17:04:00 +0200

    add types

---
+++ src/types/note.tsx
@@ -0,0 +1,24 @@
+export type Note = {
+  id: string;
+} & NoteData;
+
+export type NoteData = {
+  title: string;
+  markdown: string;
+  tags: Tag[];
+};
+
+export type RawNote = {
+  id: string;
+} & NoteData;
+
+export type RawNoteData = {
+  title: string;
+  markdown: string;
+  tagIds: string[];
+};
+
+export type Tag = {
+  id: string;
+  label: string;
+};
