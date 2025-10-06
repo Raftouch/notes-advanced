b9d39bf070e0aed9786d56de404a62001dc2c9a9
raftouch <rafanelkin@gmail.com>
2025-10-06 15:29:31 +0200

    update new note page to save notes in local storage

--- src/pages/NewNotePage.tsx
+++ src/pages/NewNotePage.tsx
@@ -1,10 +1,43 @@
+import { useMemo } from "react";
 import NoteForm from "../elements/NoteForm";
+import useLocalStorage from "../hooks/useLocalStorage";
+import type { NoteData, RawNote, Tag } from "../types/note";
+import { v4 as uuidV4 } from "uuid";
 
 export default function NewNotePage() {
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);
+
+  const notesWithTags = useMemo(() => {
+    return notes.map((note) => {
+      return {
+        ...notes,
+        tags: tags.filter((tag) => note.tagIds.includes(tag.id)),
+      };
+    });
+  }, [notes, tags]);
+
+  function onCreateNote({ tags, ...data }: NoteData) {
+    setNotes((prevNotes) => {
+      return [
+        ...prevNotes,
+        { ...data, id: uuidV4(), tagIds: tags.map((tag) => tag.id) },
+      ];
+    });
+  }
+
+  function addTag(tag: Tag) {
+    setTags((prev) => [...prev, tag]);
+  }
+
   return (
     <div className="p-10">
       <h1 className="pb-5 font-bold text-lg">New Note</h1>
-      <NoteForm />
+      <NoteForm
+        onSubmit={onCreateNote}
+        onAddTag={addTag}
+        availableTags={tags}
+      />
     </div>
   );
 }
