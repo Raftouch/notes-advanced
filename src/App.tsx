c712ee59044cfeaab4ed679abec298cacc80d070
raftouch <rafanelkin@gmail.com>
2025-10-09 17:19:31 +0200

    update note form, add note list page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import NewNotePage from "./pages/NewNotePage";
 import EditNotePage from "./pages/EditNotePage";
+import NoteList from "./pages/NoteListPage";
+import useLocalStorage from "./hooks/useLocalStorage";
+import type { Tag } from "./types/note";
 
 function App() {
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
+
   return (
     <Routes>
-      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/" element={<NoteList availableTags={tags} />} />
       <Route path="/new" element={<NewNotePage />} />
       <Route path="/:id">
         <Route index element={<h1>Show</h1>} />
--- src/pages/EditNotePage.tsx
+++ src/pages/EditNotePage.tsx
@@ -4,7 +4,7 @@ export default function EditNotePage() {
   return (
     <div className="p-10">
       <h1 className="pb-5 font-bold text-lg">Edit Note</h1>
-      <NoteForm />
+      {/* <NoteForm /> */}
     </div>
   );
 }
