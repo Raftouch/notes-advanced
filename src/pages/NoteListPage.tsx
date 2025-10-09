99f30c1e2adbc30fb6f535d0f5c8767dc1dea834
raftouch <rafanelkin@gmail.com>
2025-10-09 17:01:46 +0200

    add note list page

---
+++ src/pages/NoteListPage.tsx
@@ -0,0 +1,72 @@
+import { Link } from "react-router-dom";
+import ReactSelect from "react-select";
+import type { Tag } from "../types/note";
+import { useState } from "react";
+
+interface NoteListProps {
+  availableTags: Tag[];
+}
+
+export default function NoteList({ availableTags }: NoteListProps) {
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+  const [title, setTitle] = useState("");
+
+  return (
+    <div className="p-10">
+      <div className="flex items-center justify-between">
+        <h1 className="pb-5 font-bold text-lg">Notes</h1>
+
+        <div className="flex gap-5 mb-5">
+          <Link to="/new">
+            <button className="py-2 px-4 bg-blue-500 text-white rounded-md cursor-pointer">
+              Create
+            </button>
+          </Link>
+          {/* <Link to="/new"> */}
+          <button className="py-2 px-4 bg-orange-500 text-white rounded-md cursor-pointer">
+            Edit
+          </button>
+          {/* </Link> */}
+        </div>
+      </div>
+
+      <form>
+        <div className="flex gap-5">
+          <div className="w-1/2 flex flex-col space-y-2 pb-8">
+            <label htmlFor="title">Title</label>
+            <input
+              className="border border-gray-300 rounded min-w-[2px] py-[6px] px-3 text-gray-500"
+              type="text"
+              id="title"
+              name="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
+          <div className="w-1/2 space-y-2">
+            <label htmlFor="tags">Tags</label>
+            <ReactSelect
+              id="tags"
+              name="tags"
+              value={selectedTags.map((tag) => {
+                return { label: tag.label, value: tag.id };
+              })}
+              options={availableTags.map((tag) => {
+                return { label: tag.label, value: tag.id };
+              })}
+              onChange={(tags) => {
+                setSelectedTags(
+                  tags.map((tag) => {
+                    return { label: tag.label, id: tag.value };
+                  })
+                );
+              }}
+              isMulti
+              required
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
