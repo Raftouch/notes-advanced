8a534c285c82689dbb3253948366b7c2a103ae16
raftouch <rafanelkin@gmail.com>
2025-10-10 20:24:59 +0200

    fix notes with tags, display note list

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,27 @@ import NewNotePage from "./pages/NewNotePage";
 import EditNotePage from "./pages/EditNotePage";
 import NoteList from "./pages/NoteListPage";
 import useLocalStorage from "./hooks/useLocalStorage";
-import type { Note, Tag } from "./types/note";
+import type { RawNote, Tag } from "./types/note";
+import { useMemo } from "react";
 
 function App() {
   const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
-  const [notes, setNotes] = useLocalStorage<Note[]>("NOTES", []);
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);
+
+  const notesWithTags = useMemo(() => {
+    return notes.map((note) => {
+      return {
+        ...note,
+        tags: tags.filter((tag) => note.tagIds.includes(tag.id)),
+      };
+    });
+  }, [notes, tags]);
 
   return (
     <Routes>
       <Route
         path="/"
-        element={<NoteList availableTags={tags} notes={notes} />}
+        element={<NoteList availableTags={tags} notes={notesWithTags} />}
       />
       <Route path="/new" element={<NewNotePage />} />
       <Route path="/:id">
