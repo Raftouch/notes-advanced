31c4308dc58fd21da6c7c93812858ae619c35be1
raftouch <rafanelkin@gmail.com>
2025-10-26 16:38:21 +0100

    add edit note logic

--- src/pages/EditNotePage.tsx
+++ src/pages/EditNotePage.tsx
@@ -1,13 +1,38 @@
 import NoteForm from "../elements/NoteForm";
+import useLocalStorage from "../hooks/useLocalStorage";
 import { useNote } from "../layouts/NoteLayout";
+import type { NoteData, RawNote, Tag } from "../types/note";
 
 export default function EditNotePage() {
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);
+
   const note = useNote();
+
+  function onUpdateNote(id: string, { tags, ...data }: NoteData) {
+    setNotes((prevNotes) => {
+      return prevNotes.map((note) => {
+        if (note.id === id) {
+          return { ...note, ...data, tagIds: tags.map((tag) => tag.id) };
+        } else {
+          return note;
+        }
+      });
+    });
+  }
+
+  function addTag(tag: Tag) {
+    setTags((prev) => [...prev, tag]);
+  }
 
   return (
     <div className="p-10">
-      <h1 className="pb-5 font-bold text-lg">Edit Note for id {note.id}</h1>
-      {/* <NoteForm /> */}
+      <h1 className="pb-5 font-bold text-lg">Edit Note</h1>
+      <NoteForm
+        onSubmit={(data) => onUpdateNote(note.id, data)}
+        onAddTag={addTag}
+        availableTags={tags}
+      />
     </div>
   );
 }
